5c8eca71be5be96a6351a7600a9420a44ea6b23d
soumik12345 <19soumik.rakshit96@gmail.com>
2022-04-26 19:36:21 +0530

    updated tests

--- tests.py
+++ tests.py
@@ -5,15 +5,18 @@ from image_retrieval.dataloaders import Flickr8kDataset
 
 
 class Flickr8KTester(unittest.TestCase):
-
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName)
         self.target_size = 224
         self.artifact_id = "geekyrakshit/clip-image-retrieval/flickr-8k:latest"
         self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-    
+
     def test_dataset(self):
-        dataset = Flickr8kDataset(artifact_id=self.artifact_id, target_size=self.target_size, tokenizer=self.tokenizer)
+        dataset = Flickr8kDataset(
+            artifact_id=self.artifact_id,
+            target_size=self.target_size,
+            tokenizer=self.tokenizer,
+        )
         image, caption, input_ids, attention_mask = dataset[0]
         assert isinstance(caption, str)
         assert image.shape == (3, 224, 224)
